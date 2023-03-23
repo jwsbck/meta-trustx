gyroidos/meta-gyroidos

All recipes: Automatic srcuri syntax conversion.
Yocto kirkstone build fails if no branch or no protocol is set in git
URLs, thus conversion is required. Dunfell is oblivious to these
changes. The conversion was done using the script from Poky kirkstone under
poky/scripts/contrib/convert-srcuri.py.

Signed-off-by: Johannes Wiesboeck <johannes.wiesboeck@aisec.fraunhofer.de>

recipes-protobuf/protobuf-c-text/protobuf-c-text_git.bb
@@ -11,8 +11,8 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=c6a81c7d46ef4188c8bd511266895d5d"
 BRANCH = "master"
 
 SRCREV = "${AUTOREV}"
-#SRC_URI = "git://github.com/protobuf-c/protobuf-c-text.git;protocol=http"
-SRC_URI = "git://github.com/trustm3/external_protobuf-c-text.git;protocol=http;branch=${BRANCH}"
+#SRC_URI = "git://github.com/protobuf-c/protobuf-c-text.git;branch=master;protocol=https"
+SRC_URI = "git://github.com/trustm3/external_protobuf-c-text.git;branch=${BRANCH};protocol=https"
 
 inherit pkgconfig autotools-brokensep
 

jwsbck/meta-trustx

recipes-protobuf/protobuf-c-text/protobuf-c-text_git.bb
@@ -11,8 +11,8 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=c6a81c7d46ef4188c8bd511266895d5d"
 BRANCH = "master"
 
 SRCREV = "${AUTOREV}"
-#SRC_URI = "git://github.com/protobuf-c/protobuf-c-text.git;protocol=http"
-SRC_URI = "git://github.com/gyroidos/external_protobuf-c-text;protocol=http;branch=${BRANCH}"
+#SRC_URI = "git://github.com/protobuf-c/protobuf-c-text.git;branch=master;protocol=https"
+SRC_URI = "git://github.com/gyroidos/external_protobuf-c-text.git;branch=${BRANCH};protocol=https"
 
 inherit pkgconfig autotools-brokensep
 